4df370d50abfa8bda8e6dc2367471b3c556b62ce
FraCanta <you@example.comfcantale14@gmail.com>
2023-12-01 13:15:52 +0100

    forms

--- pages/degustazioni/[title].js
+++ pages/degustazioni/[title].js
@@ -52,7 +52,7 @@ const SingleDeg = ({ deg }) => {
               onClick={handleDrawerToggle}
               className="text-center capitalize font-medium py-2.5 px-6 2xl:py-2 2xl:px-6 fxl:py-4 fxl:px-6 3xl:py-6 3xl:px-8 2xl:text-[1.2rem] fxl:text-2xl 3xl:text-3xl rounded-[32px] text-main hover:transition-all  bg-white max-w-max "
             >
-              Prenota subito
+              Prenota ora
             </button>
           </div>
         </div>
