b99dbd9f7df6da00994cf06a35187ab47c8413bd
FraCanta <you@example.comfcantale14@gmail.com>
2024-04-05 16:34:36 +0200

    cursor

--- components/contactForm/ContactForm.js
+++ components/contactForm/ContactForm.js
@@ -7,45 +7,48 @@ const ContactForm = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div>
           {" "}
-          <span className="uppercase text-sm text-main font-bold">Nome</span>
+          <span className=" text-sm text-main font-bold">Nome</span>
           <input
             className="w-full bg-second text-main mt-2 p-3 rounded-md focus:outline-none focus:shadow-outline"
             type="text"
-            placeholder=""
+            placeholder="Mario"
           />
         </div>
         <div>
-          <span className="uppercase text-sm text-main font-bold">Cognome</span>
+          <span className=" text-sm text-main font-bold">Cognome</span>
           <input
             className="w-full bg-second text-main mt-2 p-3 rounded-md focus:outline-none focus:shadow-outline"
             type="text"
-            placeholder=""
+            placeholder="Rossi"
           />
         </div>
       </div>
       <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div>
           {" "}
-          <span className="uppercase text-sm text-main font-bold">Email</span>
+          <span className=" text-sm text-main font-bold">Email</span>
           <input
             className="w-full bg-second text-main mt-2 p-3 rounded-md focus:outline-none focus:shadow-outline"
             type="text"
+            placeholder="mariorossi@gmail.com"
           />
         </div>
         <div>
           {" "}
-          <span className="uppercase text-sm text-main font-bold">
-            Telefono
-          </span>
+          <span className=" text-sm text-main font-bold">Telefono</span>
           <input
             className="w-full bg-second text-main mt-2 p-3 rounded-md focus:outline-none focus:shadow-outline"
             type="text"
+            placeholder="+39 XXXXXXXXX"
           />
         </div>
       </div>
       <div className="mt-8">
-        <span className="uppercase text-sm text-main font-bold">Message</span>
-        <textarea className="w-full h-32 bg-second text-main mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+        <span className="text-sm text-main font-bold">Message</span>
+        <textarea
+          placeholder="scrivi qui"
+          className="w-full h-32 bg-second text-main mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+        ></textarea>
       </div>
       <div className="mt-8">
         <button className="flex items-center justify-center gap-2 capitalize font-bold py-2.5 px-6 2xl:py-2 2xl:px-6 fxl:py-4 fxl:px-6 3xl:py-6 3xl:px-8 xl:text-[1rem] 2xl:text-[1.2rem]  fxl:text-2xl 3xl:text-3xl rounded-[32px] shadow  text-white hover:transition-all  bg-[#4A4A49]">
