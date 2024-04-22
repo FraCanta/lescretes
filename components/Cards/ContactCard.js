a9bbfe40a6af48981cfe4f0f0af82b8c40557246
FraCanta <you@example.comfcantale14@gmail.com>
2024-04-22 16:09:36 +0200

    font

--- components/Cards/ContactCard.js
+++ components/Cards/ContactCard.js
@@ -5,7 +5,7 @@ const ContactCard = ({ icon, title, description, color }) => {
   return (
     <div>
       <div className="flex  w-full gap-4">
-        <div className="rounded-full bg-main text-white p-8 lg:p-5 aspect-square w-8 lg:w-16 flex items-center justify-center font-regular text-[22px] relative">
+        <div className="rounded-full bg-main text-white p-7 lg:p-4 aspect-square w-6 h-6 lg:w-16 lg:h-16 flex items-center justify-center font-regular text-[22px] relative">
           <Icon
             icon={icon}
             width={30}
