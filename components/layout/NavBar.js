9f7c0ee4603c1c438d9bd86fc7523427aa6cc25f
FraCanta <you@example.comfcantale14@gmail.com>
2024-07-05 23:06:03 +0200

    menu mobile

--- components/layout/NavBar.js
+++ components/layout/NavBar.js
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Logo from "@/public/logo/logo.png";
 import Mobile from "./mobile";
+import { motion, AnimatePresence } from "framer-motion";
+
 import CtaPrimary from "../Cta/CtaPrimary";
 import { Icon } from "@iconify/react";
 import CtaOutlineBrown from "../Cta/CtaOutlineBrown";
@@ -15,7 +17,7 @@ const NavBar = ({ translation }) => {
   return (
     <header className="relative">
       <nav className="h-[60px] md:h-[100px] lg:h-[70px] xl:h-[90px] fxl:h-[100px] 3xl:h-[180px] 4xl:h-[250px] 3xl flex w-full items-center justify-between relative z-[999999] nav-scroll ">
-        <div className="grid grid-cols-2 lg:grid-cols-5 w-[90%] mx-auto justify-between items-center ">
+        <div className="flex w-[90%] mx-auto justify-between items-center">
           <div>
             <Link href={`/`} title="Torna all'homepage di Les Crêtes">
               <Image
@@ -25,73 +27,80 @@ const NavBar = ({ translation }) => {
               />
             </Link>
           </div>
-
-          <div className="xl:flex items-center hidden capitalize flex-1 font-black text-[#4A4A49] col-span-3">
-            <div className="flex items-center justify-center w-full">
-              <div className=" group">
+          <div className="xl:flex items-center hidden font-black text-[#4A4A49] ">
+            <div className="flex items-center gap-8">
+              <div className="group">
                 <button
                   onClick={() => setMenuOpen(!menuOpen)}
-                  className="mr-[2.35rem] 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center"
+                  className=" text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular flex items-center"
                 >
                   {translation?.[locale]?.storia.name}{" "}
                   <Icon icon="ei:chevron-down" className="w-6 h-6" />
                 </button>
-                {menuOpen && (
-                  <div className="absolute top-[80px] left-0 right-0  w-full h-[50vh]   mt-2 bg-white  transition-all p-6">
-                    <div className="w-[70%] mx-auto grid grid-cols-2 justify-center h-full items-center ">
-                      <div className="flex flex-col gap-6 text-2xl">
-                        {translation?.[locale]?.storia.sottomenu.map(
-                          (item, index) => (
-                            <div
-                              key={index}
-                              className="cursor-pointer"
-                              onMouseEnter={() => setHoveredItem(index)}
-                              onMouseLeave={() => setHoveredItem(null)}
-                            >
-                              <Link href={item.link} title={item.title}>
-                                {item.name}
-                              </Link>
-                            </div>
-                          )
-                        )}
+                <AnimatePresence>
+                  {menuOpen && (
+                    <motion.div
+                      initial={{ y: "-100%" }}
+                      animate={{ y: 0 }}
+                      exit={{ y: "-100%" }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 300,
+                        damping: 30,
+                      }}
+                      className="absolute top-0 left-0 right-0 mt-[80px] w-full h-[50vh]  bg-white  transition-all p-6"
+                    >
+                      <div className="w-[90%] mx-auto grid grid-cols-2 justify-center h-full items-center ">
+                        <div className="flex flex-col gap-6 text-2xl">
+                          {translation?.[locale]?.storia.sottomenu.map(
+                            (item, index) => (
+                              <div
+                                key={index}
+                                className="cursor-pointer"
+                                onMouseEnter={() => setHoveredItem(index)}
+                                onMouseLeave={() => setHoveredItem(null)}
+                              >
+                                <Link href={item.link} title={item.title}>
+                                  {item.name}
+                                </Link>
+                              </div>
+                            )
+                          )}
+                        </div>
+                        <div className="relative flex justify-end w-full h-full py-6">
+                          {hoveredItem !== null && (
+                            <Image
+                              src={
+                                translation?.[locale]?.storia.sottomenu[
+                                  hoveredItem
+                                ].imgSrc
+                              }
+                              alt={
+                                translation?.[locale]?.storia.sottomenu[
+                                  hoveredItem
+                                ].name
+                              }
+                              fill
+                              className="object-cover"
+                            />
+                          )}
+                        </div>
                       </div>
-                      <div className="relative flex justify-end w-full h-full py-6">
-                        {hoveredItem !== null && (
-                          <Image
-                            src={
-                              translation?.[locale]?.storia.sottomenu[
-                                hoveredItem
-                              ].imgSrc
-                            }
-                            alt={
-                              translation?.[locale]?.storia.sottomenu[
-                                hoveredItem
-                              ].name
-                            }
-                            fill
-                            className="object-cover"
-                          />
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                )}
+                    </motion.div>
+                  )}
+                </AnimatePresence>
               </div>
               <Link
                 href={`/i-vini`}
                 title="I nostri vini"
-                className={`mr-[2.35rem] 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center ${
-                  pathname === "/i-vini"
-                    ? "underline underline-offset-4 decoration-main"
-                    : ""
-                }`}
+                className="text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center"
               >
                 {translation?.[locale]?.vini}
               </Link>
               <Link
                 href={`/le-degustazioni`}
                 title="Le nostre degustazioni"
-                className={`mr-[2.35rem] 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center${
+                className={`text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center${
                   pathname === "/i-vini"
                     ? "underline underline-offset-4 decoration-main"
                     : ""
@@ -99,34 +108,25 @@ const NavBar = ({ translation }) => {
               >
                 {translation?.[locale]?.degustazioni}
               </Link>
-              {/* <Link
-                href={`/notizie`}
-                title="I miei articoli"
-                className="mr-[2.35rem] 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center"
-              >
-                {translation?.[locale]?.news}
-              </Link> */}
+
               <Link
                 href={`/cosmesi`}
                 title="La nostra cosmesi"
-                className="mr-[2.35rem] 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center"
+                className="text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center"
               >
                 {translation?.[locale]?.cosmesi}
               </Link>
-              <Link
-                href={`/store-locator`}
-                title="Dove poter acquistare"
-                className="gap-1 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-start"
-              >
-                <Icon
-                  icon="line-md:map-marker-alt-filled"
-                  className="w-6 h-6"
-                />
-                {translation?.[locale]?.store}
-              </Link>
             </div>
           </div>
-          <div className="items-center justify-end flex-1 hidden font-black xl:flex">
+
+          <div className="items-center justify-end hidden gap-8 font-black xl:flex">
+            {/* <Link
+              href={`/notizie`}
+              title="I miei articoli"
+              className="text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center"
+            >
+              {translation?.[locale]?.news}
+            </Link> */}
             {/* <Link
               href={`/areaDownload`}
               title="I miei articoli"
@@ -149,9 +149,21 @@ const NavBar = ({ translation }) => {
               </svg>
             </Link> */}
 
-            <CtaOutlineBrown link="/contatti" title="Pagina info e contatti">
+            <Link
+              href="/contatti"
+              title="Pagina info e contatti"
+              className="mr-[1rem] gap-2 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-center"
+            >
               {translation?.[locale]?.contatti}
-            </CtaOutlineBrown>
+            </Link>
+            <Link
+              href={`/store-locator`}
+              title="Dove poter acquistare"
+              className="gap-1 3xl:mr-12 4xl:mr-16 text-[16px] md:text-[1.2rem] xl:text-[1.2rem] fxl:text-[25px]  3xl:text-[35px] 4xl:text-[55px]  text-main font-regular capitalize flex items-start"
+            >
+              <Icon icon="line-md:map-marker-alt-filled" className="w-6 h-6" />
+              {translation?.[locale]?.store}
+            </Link>
           </div>
           <div className="flex items-center justify-end py-1 text-main xl:hidden ">
             <Mobile translation={translation} />
