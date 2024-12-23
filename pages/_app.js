30ee376e7b05416a99cf6d38f810cfc8d2cfc0ae
FraCanta <fcantale14@gmail.com>
2024-12-23 21:13:56 +0100

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -68,6 +68,14 @@ export default function App({ Component, pageProps, router }) {
           gtag('config', 'G-N78X2SJWPF', { 'debug_mode': true });
         `}
           </Script>
+          {/* Google Tag Manager */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','G-N78X2SJWPF');`}
+          </Script>
         </main>
       </motion.div>
     </AnimatePresence>
