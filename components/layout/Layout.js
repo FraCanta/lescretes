8b12c3d60d153f5d72969b8eac2fc5c547e8d563
FraCanta <fcantale14@gmail.com>
2024-12-23 16:34:16 +0100

    email

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import LayoutTranslation from "../../public/layout.json";
 import Footer from "./Footer";
 import { Toaster } from "react-hot-toast";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 export const Layout = (props) => {
   return (
@@ -27,6 +28,17 @@ export const Layout = (props) => {
       />
       <NavBar translation={LayoutTranslation?.menu} />
       <main>{props.children}</main>
+      <FloatingWhatsApp
+        phoneNumber="+390165902274"
+        avatar="/apple-touch-icon.png"
+        accountName="Les Cretes"
+        statusMessage="Ti risponderemo al più presto"
+        allowClickAway
+        notification
+        chatMessage="Ciao, come possiamo aiutarti?"
+        notificationSound={true}
+        placeholder="Scrivi un messaggio..."
+      />
       <Footer translation={LayoutTranslation?.footer} />
     </>
   );
