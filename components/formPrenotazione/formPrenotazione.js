549513ec12461ef4be78b5c563841969b9c22285
FraCanta <fcantale14@gmail.com>
2025-01-02 21:36:54 +0100

    pagina regalo e form completati

--- components/formPrenotazione/formPrenotazione.js
+++ components/formPrenotazione/formPrenotazione.js
@@ -508,7 +508,7 @@ const FormPrenotazione = ({
             >
               Acquista <Icon icon="icons8:buy" className="w-6 h-full" />
             </button> */}
-            <button
+            {/* <button
               type="submit"
               className={`cursor-pointer flex items-center text-lg xl:text-xl gap-2 text-main w-full max-w-max text-center lg:text-[21.57px] font-bold leading-snug py-2.5 px-6 2xl:py-2 2xl:px-6 fxl:py-4 fxl:px-6 3xl:py-6 3xl:px-8 2xl:text-[1.2rem] fxl:text-2xl 3xl:text-3xl rounded-[32px] border-2 ${
                 timeSlot && clickedRadio
@@ -518,7 +518,15 @@ const FormPrenotazione = ({
               disabled={!timeSlot || !clickedRadio}
             >
               {form.cta}
-            </button>
+            </button> */}
+            {timeSlot && clickedRadio && (
+              <button
+                type="submit"
+                className="cursor-pointer flex items-center text-lg xl:text-xl gap-2 text-main w-full max-w-max text-center lg:text-[21.57px] font-bold leading-snug py-2.5 px-6 2xl:py-2 2xl:px-6 fxl:py-4 fxl:px-6 3xl:py-6 3xl:px-8 2xl:text-[1.2rem] fxl:text-2xl 3xl:text-3xl rounded-[32px] border-2 border-main"
+              >
+                {form.cta}
+              </button>
+            )}
           </div>
         </form>
       </div>
