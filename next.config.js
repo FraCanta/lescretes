76fa3c25b3d7ae21e7066154eab222cd5ba0ca1c
FraCanta <fcantale14@gmail.com>
2025-02-26 17:25:27 +0100

    lingue

--- next.config.js
+++ next.config.js
@@ -28,13 +28,13 @@ const nextConfig = {
       },
       // Reindirizzamento per /en/our-wines con categoria dinamica
       {
-        source: "/en/our-wines/:categoria/:prodotto",
-        destination: "https://www.lescretes.it/en/i-vini/:categoria/:prodotto", // Usa :categoria per mappare la categoria dinamica
+        source: "/en/our-wines/:categoria/*",
+        destination: "https://www.lescretes.it/en/i-vini/:categoria/*", // Usa :categoria per mappare la categoria dinamica
         permanent: true,
       },
       {
-        source: "/fr/nos-vins/:categoria/:prodotto",
-        destination: "https://www.lescretes.it/fr/i-vini/:categoria/:prodotto", // Usa :categoria per mappare la categoria dinamica
+        source: "/fr/nos-vins/:categoria/*",
+        destination: "https://www.lescretes.it/fr/i-vini/:categoria/*", // Usa :categoria per mappare la categoria dinamica
         permanent: true,
       },
       {
