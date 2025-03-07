4435b81f5935ad8bb4e3b8c7fa3cfec1896fbac5
FraCanta <fcantale14@gmail.com>
2025-03-07 19:44:49 +0100

    headers security

--- next.config.js
+++ next.config.js
@@ -97,7 +97,7 @@ const ContentSecurityPolicy = `
   child-src 'self' https://www.iubenda.com/ http://embeds.iubenda.com/;
   style-src 'self' https://p.typekit.net/ https://fonts.googleapis.com/ https://use.typekit.net/ https://cdn.iubenda.com/ 'unsafe-inline' data:;
   font-src 'self' https://use.typekit.net/ https://fonts.gstatic.com/ 'unsafe-inline' data:;
-  img-src 'self' https://a.tile.openstreetmap.org/ https://cdnjs.cloudflare.com/ https://lh3.googleusercontent.com/ data: blob:;
+  img-src 'self' https://c.tile.openstreetmap.org/ https://a.tile.openstreetmap.org/ https://cdnjs.cloudflare.com/ https://lh3.googleusercontent.com/ data: blob:;
   media-src 'self' data:audio/mpeg; ;
 `;
 
